db6309394706d6a0eb082f62bd25298b378f8f7e
jihyelucky <94450652+jihyelucky@users.noreply.github.com>
2022-06-09 11:45:04 +0900

    add resetBtn EraserBtn

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
+const resetBtn = document.getElementById("jsReset");
+const eraserBtn = document.getElementById("jsEraser");
 
 const INITIAL_COLOR = "#2c2c2c";
-const CANVAS_SIZE = 700;
+const CANVAS_SIZE = 500;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
@@ -71,11 +73,21 @@ function handleCM(event) {
 }
 
 function handleSaveClick() {
-    const image = canvas.toDataURL();
-    const link = document.createElement("a");
-    link.href = image;
-    link.download = "PaintJS[🎨]";
-    link.click();
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS[🎨]";
+  link.click();
+}
+
+function handleCanvasReset() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+
+function handleCanvasEraser() {
+  ctx.strokeStyle = "white";
+  console.log("aaaa");
 }
 
 if (canvas) {
@@ -101,4 +113,12 @@ if (mode) {
 
 if (saveBtn) {
   saveBtn.addEventListener("click", handleSaveClick);
+}
+
+if (resetBtn) {
+  resetBtn.addEventListener("click", handleCanvasReset);
+}
+
+if (eraserBtn) {
+  eraserBtn.addEventListener("click", handleCanvasEraser);
 }
